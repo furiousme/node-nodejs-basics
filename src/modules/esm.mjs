ca55a67707145e3ba4bb73d1e638c4254b844891
furiousme <shuptaroks@gmail.com>
2024-10-04 00:28:17 +0300

    feat: implemented esm module

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,26 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'node:path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'http';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
+
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
+const readJson = async (pathToFile) => {
+    return JSON.parse(await readFile(__dirname + pathToFile));
+}
+
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await readJson("/files/a.json");
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await readJson("/files/b.json")
 }
 
 console.log(`Release ${release()}`);
@@ -33,7 +43,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export default {
     unknownObject,
     myServer,
 };
