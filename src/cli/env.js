b6ab3592917143dacc32e55e60c0534b170e9725
furiousme <shuptaroks@gmail.com>
2024-10-04 00:16:50 +0300

    feat: implement functions for cli module

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,25 @@
 const parseEnv = () => {
-    // Write your code here 
+    const args = process.argv;
+    let n = 2;
+    let result = "";
+
+    while (n < args.length) {
+        if (args[n].startsWith("RSS_")) {
+            const argName = args[n];
+            const value = args[n + 1];
+            
+            if (value == undefined) {
+                throw new Error(`Value for the argument ${argName} was not provided.`)
+            }
+
+            result = `${result}${result ? ";" : ""} ${args[n]}=${value}`;
+            n += 2;
+
+            continue;
+        }
+    }
+
+    console.log(result);
 };
 
 parseEnv();
