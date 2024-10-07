3d0afd29fada126aa3fd4fd264ab9eb89537a65b
furiousme <shuptaroks@gmail.com>
2024-10-07 18:35:23 +0300

    feat: add functions for wt module

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { Worker } from 'worker_threads';
+import os, { cpus } from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+   const results = [];
+   const cpusNum = os.cpus().length;
+   let counter = 0;
+
+   for (let i = 0; i < cpusNum; i++) {
+        const worker = new Worker(__dirname + "/worker.js");
+
+        worker.once('message', (message) => {
+            results[i] = message;
+            if (cpusNum - counter === 1) {
+                console.log(results);
+                process.exit()
+            } else {
+                counter++;
+            }
+        });
+
+        worker.postMessage(10 + i);
+   }
 };
 
 await performCalculations();
