543e5273c01e69802024d7f0502052b11926aabe
furiousme <shuptaroks@gmail.com>
2024-10-07 20:36:45 +0300

    feat: implement functions for cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { fork } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork(__dirname + "/files/script.js", args);
+
+    child.on('error', (e) => {
+        console.log(e);
+        process.exit(1);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["first arg", "second arg"]);
